9800941e42d8ac92af19fdf2a6179b2ac6d02ef8
John Seng <jseng@calpoly.edu>
2013-10-30 19:48:52 -0700

    build installer for Linux with Aithon library included

--- aithon_plugin/jeditBuild/buildinstallers.sh
+++ aithon_plugin/jeditBuild/buildinstallers.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+#run on Mac or Linux
 
 #checkout v.5.1.1 of jEdit
-svn co https://jedit.svn.sourceforge.net/svnroot/jedit/jEdit/branches/5.1.x jedit_temp
+svn co -r 23300 https://jedit.svn.sourceforge.net/svnroot/jedit/jEdit/branches/5.1.x jedit_temp
 
+cd jedit_temp
+cp -r ../../../AithonLibrary .
+#set +e #continue even with error
+ant dist
+#set -e
+
+#copy over the most recent versions of 'ld' and 'windres'
+cp ../ld-new ./build/launch4j/bin/ld
+cp ../windres ./build/launch4j/bin/windres
+
+cp ../build.xml.modified_5.1.1 ./build.xml
+ant dist
 
 
