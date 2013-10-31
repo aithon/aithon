9108ab6097c2973e5b1af2eeec46985896e2c2af
John Seng <jseng@jseng.csc.calpoly.edu>
2013-10-31 12:13:47 -0700

    added copy of splash screen

--- jeditBuild/buildinstallers.sh
+++ jeditBuild/buildinstallers.sh
@@ -10,13 +10,17 @@ cp -r ../../AithonLibrary .
 ant dist
 #set -e
 
-#copy over the most recent versions of 'ld' and 'windres'
+#copy over the most recent versions of 'ld' and 'windres' (needed for Mac installer)
 cp ../ld-new ./build/launch4j/bin/ld
 cp ../windres ./build/launch4j/bin/windres
 
+#copy the modified build.xml
 cp ../build.xml.modified_5.1.1 ./build.xml
 
-#need for InnoSetup
+#copy the splash image
+cp ../splash_aithon.png ./org/gjt/sp/jedit/icons/splash.png
+
+#need for InnoSetup (needed for Windows installer)
 cp ../build.properties ./build.properties
 cp ../win32installer.iss ./package-files/windows/win32installer.iss
 
