fb521ad16cd40ba7d81c202eeb748676fcb85af2
John Seng <jseng@calpoly.edu>
2014-02-20 21:45:29 -0800

    files for embedded bootloader

---
+++ Bootloader/embed_bootloader/embed.py
@@ -0,0 +1,37 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+#this script reads in a .bin file and outputs a C header file that can
+#be included in a user application
+
+import sys
+import re
+import binascii
+
+lines = 0
+count = 0
+temp_byte = bytearray()
+
+print "static const int bootloader[8192] = {"
+
+#output binary
+f = open(sys.argv[1], "rb")
+try:
+    byte = f.read(4)
+    while byte != "":
+      print "%s%s %s" % ("0x", binascii.hexlify(bytearray(byte)), " ,")
+      if lines == 1: #check that it is a bootloader binary
+        temp_byte = bytearray(byte)
+        temp_byte.reverse()
+        reset_addr = int(binascii.hexlify(temp_byte), 16)
+        if reset_addr > 0x08010000:
+          print "invalid reset address - this is a user application binary"
+          sys.exit()
+      byte = f.read(4)
+      lines = lines + 1
+finally:
+  f.close()
+
+
+print "0 };"
+print "static const int lines = " + str(lines+1) + ";"
+
---
+++ Bootloader/embed_bootloader/main.h
@@ -0,0 +1,35 @@
+#include "Aithon.h"
+#include "flash_if.h"
+
+// general constants
+#define PACKET_LEN         1024
+#define BOOT_TIMEOUT       30000
+#define DEFAULT_TIMEOUT    1000
+
+
+// Commands
+#define SYNC               0x01
+#define ERASE_FLASH_START  0x1B
+#define ERASE_FLASH_STATUS 0x09
+#define SET_ADDR           0x1A
+#define CHECK_ADDR         0x3B
+#define FILL_BUFFER        0x29
+#define CHECK_BUFFER       0x06
+#define COMMIT_BUFFER      0x28
+#define START_PROGRAM      0x11
+
+// Responses
+#define ACK                0x40
+#define NACK               0x80
+#define BUSY               0xC0
+
+
+// function pointer used for jumping to the user program
+typedef void (*funcPtr)(void);
+
+// all the interfaces that programming is supported over
+#define NUM_INTERFACES  3
+SerialDriver *_interfaces[NUM_INTERFACES] = {&SD1, &SD2, (SerialDriver *)&SDU1};
+SerialDriver *_interface;
+
+uint8_t _buffer[PACKET_LEN];
